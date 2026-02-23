cd23f677117f0e2ebcbd22e13bd5ceee81a1cada
GreatEmperorAca <105924909+GreatEmperorAca@users.noreply.github.com>
2026-02-23 16:24:24 +0100

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
 import * as schema from './schema';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
